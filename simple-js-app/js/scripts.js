e1632675608db16e3114076f32afb31b317b9ce8
stefv21 <161451930+stefv21@users.noreply.github.com>
2024-08-20 08:20:51 -0400

    created a new variable

---
+++ simple-js-app/js/scripts.js
@@ -0,0 +1,11 @@
+let pokemon = 'pokemonList';
+console.log(typeof pokemon);
+
+
+let pokemonlist = [
+  { name: 'pikachu', type: 'electric', height: '7'},
+  { name: 'charmeleon', type: 'fire', height: '3'},
+  { name: 'pidgeot', type: 'normal', height: '5'}
+];
+
+console.log(pokemonlist);
